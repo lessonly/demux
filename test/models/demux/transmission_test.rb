4670d4355bde3d88d2bc4272601445b29c8b42ef
SherSpock <wrtimberlake@gmail.com>
2020-10-28 13:46:22 -0400

    older_then->older_than

--- test/models/demux/transmission_test.rb
+++ test/models/demux/transmission_test.rb
@@ -8,7 +8,7 @@ module Demux
       slack_transmission = demux_transmissions(:slack)
       purgable_transmission = demux_transmissions(:purgable_slack)
 
-      Demux::Transmission.purge(older_then: 1.month.ago)
+      Demux::Transmission.purge(older_than: 1.month.ago)
 
       assert Demux::Transmission.where(id: slack_transmission.id).exists?
       refute Demux::Transmission.where(id: purgable_transmission.id).exists?
