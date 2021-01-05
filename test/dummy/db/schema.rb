67a62c3954b027a46300bf1f7f96a98894e35bd0
Ross Reinhardt <rreinhardt9@gmail.com>
2021-01-05 15:48:35 -0500

    Add RSA access keys to Apps
    Demux access keys will be used by parent apps to authorize an app
    wishing to perform administrative tasks, such as retrieving tokens for
    it's connections or destroying existing connections.
    
    Josh Frankel <joshmfrankel@gmail.com>

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_201706) do
+ActiveRecord::Schema.define(version: 2021_01_05_192025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "demux_access_keys", force: :cascade do |t|
+    t.text "public_key", null: false
+    t.integer "app_id", null: false
+    t.string "fingerprint", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["app_id"], name: "index_demux_access_keys_on_app_id"
+  end
 
   create_table "demux_apps", force: :cascade do |t|
     t.string "name"
